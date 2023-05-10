9608c4af77ad861090f1f4a3502c9c6b1caefde4
PAD2003 <phananhduc138@gmail.com>
2023-05-10 08:12:18 +0700

    Initial commit

---
+++ test_packages.py
@@ -0,0 +1,5 @@
+from demos.apply_filter_v0.src import ApplyFilter
+
+# ApplyFilter.apply_filter_on_image("apply_filter/test/data/images/img2.png").save("b.png")
+
+ApplyFilter.apply_filter_on_video("apply_filter/test/data/videos/IMG_1842.MOV", "", None)
